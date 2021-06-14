4edd7c95f2c3f4d0921ae2460859f44356a07d33
parvardegr <somthing4die@gmail.com>
2021-06-14 14:57:41 +0430

    test dynamic import

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ import { serve } from "https://deno.land/std@0.98.0/http/server.ts";
 
 import { message } from "./depts.js"
 
+
+import("./depts.js").then(m => {
+
+  console.log("dynamic loaded");
+  console.log(m.message);
+});
+
+
 const s = serve({ port: 8000 });
 console.log("http://localhost:8000/");
 for await (const req of s) {
