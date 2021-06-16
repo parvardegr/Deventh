ca96ad477c2c3fb4958b4101fef674fc488f8f95
parvardegr <somthing4die@gmail.com>
2021-06-16 20:44:11 +0430

    loadHanlersByModulePath

--- main.js
+++ main.js
@@ -1,12 +1,51 @@
 import { serve } from "https://deno.land/std@0.98.0/http/server.ts";
 
-import { message } from "./depts.js"
+const handlers = [
+  "/opt/logger.js",
+  "/opt/queue.js",
+  "/opt/integration.js"
+  ]
 
+const handlerModulesMap = async (handlers = []) => {
+  console.log("loading handler modules");
+  console.log(handlers);
 
-import("/opt/depts.js").then(m => {
+  return handlers.map(handlerModule => {
+    try {
 
-  console.log("dynamic loaded");
-  console.log(m.message);
+      console.log("start loading handler module " + handlerModule);
+
+      const module = await import(handlerModule);
+
+      if (module.handle) {
+
+        console.log("OK: load handler " + handlerModule);
+
+        return module;
+
+      } else {
+        console.log("cant load handler module " + handlerModule);
+        console.log("cant find 'export async handle(event, context)' function in provided module " + handlerModule);
+      }
+
+    } catch (e) {
+      console.log(e);
+    }
+
+    return undefined;
+
+  }).filter(module => module !== undefined);
+
+}
+
+import("/opt/logger.js").then(m => {
+
+  m.handle({
+    "event": "TEST"
+  }, {
+    "handlerName": m.name
+  });
+  
 });
 
 
@@ -15,3 +54,5 @@ console.log("http://localhost:8000/");
 for await (const req of s) {
   req.respond({ body: message });
 }
+
+console.log(await handlerModulesMap());
